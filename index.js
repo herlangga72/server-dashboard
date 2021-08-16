9ad7f0c29ff52f7a8b133984da8603680146edcf
Herlangga Yusuf Syailendra <herlangga72@gmail.com>
2021-08-16 15:11:48 +0700

    Paging complete

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     queueLimit: 0
   })
+const promisePool = pool.promise()
 
-
-app.use(cors())
+app .use(cors())
     .use(express.json())
     .use(express.urlencoded({ extended: true })
 )
@@ -27,9 +27,7 @@ app.post('/login/', function(request, respon){
     let uname=request.body.uname
     pool.query("SELECT pass FROM login WHERE uname='"+uname+"'" , 
         function(err, rows, fields) {
-            console.log(request.body);
-            // { uname: 'herlangga', pass: '1' }
-            if (rows[0].pass==request.body.pass){
+            if (rows[0].pass===request.body.pass){
                 respon.send()
             }
             else{
@@ -39,18 +37,38 @@ app.post('/login/', function(request, respon){
     )
 })
 
+async function getUserData(dataSize, startingId, Search){
+    let SQL = "SELECT Id,uname,AccessLevel FROM login"
+    console.log("StartingID "+startingId)
+    console.log("dataSize "+ dataSize)
+    if (dataSize != null && startingId != null){
+        SQL = SQL + " WHERE Id>="+startingId+" LIMIT "+ dataSize
+    }
+    let [rows] = await promisePool.query(SQL)
+    return rows
+}
+app.post('/User/TotalData',async function(request,respon){
+    let [rows] = await promisePool.query("SELECT Value FROM information WHERE Name='login'")
+    respon.send(rows)
+}
+)
+// app.post('/User/Data',async function(request, respon){
+//     respon.header('Content-Type', 'application/json');
+//     let test=[]
+//     await getUserData(dataSize=5,startingId=1).then(baris => baris.forEach(rows => test.push({id:rows.Id,uname:rows.uname,accessLevel:rows.AccessLevel})))
+//     respon.send(test)
+// })
 
-app.post('/getdata/', function(request, respon){
+app.post('/User/Data',async function(request, respon){
+    //support pagination
+    let startingId = request.body.id
+    let dataSize   = request.body.Size
+    console.log("before "+request.body.Size);
     respon.header('Content-Type', 'application/json');
-    var test=[]
-    pool.query("SELECT Id,uname,AccessLevel FROM login", function(err, rows, fields) {
-        //this used for make data separeate with unique id. this handle error on ibm component table
-        rows.forEach(element => { 
-            test.push({id:element.Id,uname:element.uname,accessLevel:element.AccessLevel})
-        });
-        console.log(test)
-        respon.send(test)
-    })
+    let test=[]
+    await getUserData(dataSize,startingId).then(baris => baris.forEach(rows => test.push({id:rows.Id,uname:rows.uname,accessLevel:rows.AccessLevel})))
+    console.log(test);
+    respon.send(test)
 })
 
 app.listen(2000)
